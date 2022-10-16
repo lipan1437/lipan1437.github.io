de7edd68de11d19cbd9b1f4a62ac23d8966c962f
lipan padhan <lipanpadhan1432@gmail.com>
2022-10-16 14:02:33 +0530

    add nav

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className='App'>
+      <a href="#">Home</a>
+      <a href="#about">About</a>
+      <a href="#skills">Skills</a>
+      <a href="#contact">Contact</a>
+      <a href="#project">Project</a>   
+    </div>
+ 
+  );
+}
+
+export default App;
