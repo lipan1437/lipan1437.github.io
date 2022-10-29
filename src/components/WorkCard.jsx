9502e56839f4e51f97e7f9d4520d546bdbc1ba50
lipan padhan <lipanpadhan1432@gmail.com>
2022-10-29 23:29:43 +0530

    adding skills

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -1,10 +1,9 @@
 import "./WorkCardStyles.css"
 import React from 'react'
-import { NavLink } from "react-router-dom"
 const WorkCard = (props) => {
   return (
     <div className="project-card">
-                <img src={props.imgsrc} alt="image" />
+                <img src={props.imgsrc} alt="pro-thumb" />
                 <h2 className="project-title">{props.title}</h2>
                 <div className="pro-details">
                     <p>{props.text}</p>
