cc6d3087a864763b68fef3974f3d78b2bee31418
lipan padhan <lipanpadhan1432@gmail.com>
2022-10-29 23:40:09 +0530

    changes

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -1,19 +1,23 @@
-import "./WorkCardStyles.css"
-import React from 'react'
+import "./WorkCardStyles.css";
+import React from "react";
 const WorkCard = (props) => {
   return (
     <div className="project-card">
-                <img src={props.imgsrc} alt="pro-thumb" />
-                <h2 className="project-title">{props.title}</h2>
-                <div className="pro-details">
-                    <p>{props.text}</p>
-                    <div className="pro-btns">
-                        <a href={props.view}className="btn">View</a>
-                        <a href={props.source} className="btn">Source</a>
-                    </div>
-                </div>
-            </div>
-  )
-}
+      <img src={props.imgsrc} alt="pro-thumb" />
+      <h2 className="project-title">{props.title}</h2>
+      <div className="pro-details">
+        <p>{props.text}</p>
+        <div className="pro-btns">
+          <a href={props.view} className="btn">
+            View
+          </a>
+          <a href={props.source} className="btn">
+            Source
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WorkCard
+export default WorkCard;
